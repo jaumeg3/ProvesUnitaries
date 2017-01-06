dec67d9552901e00f8639f095387c8baa378cf64
Jaume Giralt Barbé <jaume.giralt.2012@gmail.com>
2017-01-06 18:09:08 +0100

    Inici Apartat 2 de la pràctica. Creació Interfície Validation Service.

---
+++ src/services/ValidationService.java
@@ -0,0 +1,13 @@
+package services;
+
+import kiosk.ActivationCard;
+
+/**
+ * Local service for validating activation cards
+ */
+
+public interface ValidationService {
+
+    boolean validate(ActivationCard card);
+    void deactivate(ActivationCard card);
+}
